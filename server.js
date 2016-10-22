e5ee1a4c83722d585fc9445e8eda2a6965de7b6c
Christopher Bruner <chrbruner9@gmail.com>
2016-10-22 13:41:21 -0700

    removed commented out code

--- server.js
+++ server.js
@@ -11,17 +11,6 @@ function start(route, handle) {
     console.log('Request for ' + pathname + ' received.');
     route(handle, pathname, response, request);
   };
-  //   request.setEncoding('utf8');
-
-  //   request.addListener('data', function(postDataChunk) {
-  //     postData += postDataChunk;
-  //     console.log('Received POST data chunk ' + postDataChunk + '.');
-  //   });
-
-  //   request.addListener('end', function() {
-  //     route(handle, pathname, response, postData);
-  //   });
-  // };
 
   http.createServer(onRequest).listen(3001);
   console.log('Server has started');
